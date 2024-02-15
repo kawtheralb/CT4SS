b3471a3949c0532e33e15b72df8d129b1c6292cb
“Kawther <kawtheralbader@arizona.edu>
2024-02-14 17:00:51 -0700

    commit

---
+++ ch4a.R
@@ -0,0 +1,15 @@
+#test
+#Type coercion
+
+"1" == 1
+"1" == 2
+1 == TRUE
+"1" == TRUE
+
+as.logical(2)
+2 == TRUE
+
+"TRUE" == TRUE
+"true" == TRUE
+
+# lists: useful when storing objects of unknown type 
